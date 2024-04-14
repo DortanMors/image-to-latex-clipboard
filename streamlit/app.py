0a9feef47b3a7f4a0486a1af3eba272518fa1c65
Fomin <vyacheslav.fomin@magenta-technology.com>
2024-04-15 00:53:36 +0400

    "From clipboard" button for streamlit app

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,7 +1,10 @@
+import io
+
 import requests
 from PIL import Image
 
 import streamlit as st
+from streamlit_paste_button import paste_image_button as pbutton
 
 
 st.set_page_config(page_title="Image To Latex Converter")
@@ -9,25 +12,29 @@ st.set_page_config(page_title="Image To Latex Converter")
 
 st.title("Image to Latex Converter")
 
-
-uploaded_file = st.file_uploader(
-    "Upload an image of latex math equation",
-    type=["png", "jpg"],
-)
-
-
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image)
-
-
-if st.button("Convert"):
-    if uploaded_file is not None and image is not None:
-        files = {"file": uploaded_file.getvalue()}
+image_data = pbutton("Воткуть из буффера обмена")
+uploaded_file = None
+# uploaded_file = st.file_uploader(
+#     "Upload an image of latex math equation",
+#     type=["png", "jpg"],
+# )
+if st.button("Получить LaTeX"):
+    if uploaded_file is not None or image_data.image_data is not None:
+        if uploaded_file is not None:
+            files = {"file": uploaded_file.getvalue()}
+        elif image_data.image_data is not None:
+            files = {"file": io.BytesIO(image_data.image_data._repr_png_())}
         with st.spinner("Wait for it..."):
-            response = requests.post("http://0.0.0.0:8000/predict/", files=files)
+            response = requests.post("http://localhost:8000/predict/", files=files)
         latex_code = response.json()["data"]["pred"]
         st.code(latex_code)
         st.markdown(f"${latex_code}$")
     else:
-        st.error("Please upload an image.")
+        st.error("Надо воткнуть фотку!")
+
+if image_data.image_data is not None:
+    st.image(image_data.image_data, caption="Загруженная имага", use_column_width=True)
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image)
